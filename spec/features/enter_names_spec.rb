3ce053c8e98749eccc84914b3a00119ac1872181
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-14 15:50:41 +0000

    sign in and play method added

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'on the home page' do
   scenario 'players enter names' do
-    visit '/'
-    fill_in :player_1_name, with: "Chris"
-    fill_in :player_2_name, with: "Asad"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'Chris vs. Asad'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'hit points' do
   scenario 'can view players hit points' do
-    visit '/'
-    fill_in :player_1_name, with: "Chris"
-    fill_in :player_2_name, with: "Asad"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Asad's HP: 60"
   end
 end
