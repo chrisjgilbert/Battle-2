0bcead71a42851b0e8377b056e50f9ea794f5734
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-14 16:02:26 +0000

    refactored

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,6 +1,7 @@
 feature 'attacking opponent' do
   scenario 'player attacks opponent' do
     sign_in_and_play
+    click_button 'player_1_attack'
     expect(page).to have_content 'Chris attacked Asad!'
   end
 end
