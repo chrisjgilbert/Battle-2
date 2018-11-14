df415d1b69a4fb784e6e00b54bf16cf8475d8a91
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-14 17:54:07 +0000

    divdied attack test into 2 seperate

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -7,6 +7,7 @@ feature 'attacking opponent' do
   scenario 'player 1 reduces player 2 HP by 10' do
     sign_in_and_play
     click_button 'Attack'
+    expect(page).not_to have_content "Asad's HP: 60"
     expect(page).to have_content "Asad's HP: 50"
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,22 +2,32 @@ require 'player'
 
 describe Player do
 
-  subject(:player_1) { described_class.new("Chris") }
-  subject(:player_2) { described_class.new("Asad") }
+  subject(:chris) { described_class.new("Chris") }
+  subject(:asad) { described_class.new("Asad") }
 
-  it 'it retuns its name' do
-    expect(player_1.name).to eq "Chris"
+  describe '#name' do
+    it 'it retuns its name' do
+      expect(chris.name).to eq "Chris"
+    end
   end
 
-  it 'has a default HP' do
-    expect(player_1.hit_points).to eq Player::DEFAULT_HP
+  describe '#hit_points' do
+    it 'has a default HP' do
+      expect(chris.hit_points).to eq described_class::DEFAULT_HP
+    end
   end
 
-  it 'can attack an opponent' do
-    expect(player_1).to respond_to :attack
+  describe '#attack' do
+    it 'attacks an opponent' do
+      expect(asad).to receive(:receive_damage)
+      chris.attack(asad)
+    end
   end
 
-  it 'reduces an opponents HP by 10' do
-    expect { player_1.attack(player_2) }.to change{ player_2.hit_points }.by -10
+  describe '#receive_damage' do
+    it 'reduces a players hit points by 10' do
+      expect { chris.receive_damage }.to change{ chris.hit_points }.by -10
+    end
   end
+
 end
