9e0d412566f914527d98066bb60fb65bd0df713c
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-14 15:28:58 +0000

    refactored

--- spec/features/app_feature_spec.rb
+++ spec/features/app_feature_spec.rb
@@ -3,7 +3,7 @@ feature 'on the home page' do
     visit '/'
     fill_in :player_1_name, with: "Chris"
     fill_in :player_2_name, with: "Asad"
-    click_button 'submit'
+    click_button 'Submit'
     expect(page).to have_content 'Chris vs. Asad'
   end
 end
