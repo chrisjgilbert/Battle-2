947a366e3a9d71e9d1c9d705dcdd4a939ba3c594
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-15 16:51:51 +0000

    refactored with helper method

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -7,13 +7,10 @@ feature 'hit points' do
 
   scenario 'can view players hit points' do
     sign_in_and_play
-    click_button 'Attack'
-    click_button 'Continue'
-    expect(page).to have_content "Asad's HP: 50"
-    expect(page).to have_content "Chris's HP: 60"
-    click_button 'Attack'
-    click_button 'Continue'
+    attack_and_continue
+    attack_and_continue
     expect(page).to have_content "Asad's HP: 50"
     expect(page).to have_content "Chris's HP: 50"
   end
+
 end
