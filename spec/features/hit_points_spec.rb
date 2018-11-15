9f3b1020c86a263aaf75ef95b5ba68df595de11a
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-15 14:38:09 +0000

    both players HP shown and descrease

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,7 +1,19 @@
 feature 'hit points' do
-  scenario 'can view players hit points' do
+  scenario 'can view players default hit points' do
     sign_in_and_play
     expect(page).to have_content "Asad's HP: 60"
     expect(page).to have_content "Chris's HP: 60"
+  end
+
+  scenario 'can view players hit points' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_button 'Continue'
+    expect(page).to have_content "Asad's HP: 50"
+    expect(page).to have_content "Chris's HP: 60"
+    click_button 'Attack'
+    click_button 'Continue'
+    expect(page).to have_content "Asad's HP: 50"
+    expect(page).to have_content "Chris's HP: 50"
   end
 end
