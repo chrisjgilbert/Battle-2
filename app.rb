b66c4be724d1df744ef69e68f0dca42bb46581ba
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-16 14:48:23 +0000

    class methods implemented

--- app.rb
+++ app.rb
@@ -11,19 +11,21 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:player_1_name])
-    $player_2 = Player.new(params[:player_2_name])
-    $game = Game.new($player_1, $player_2)
+    player_1 = Player.new(params[:player_1_name])
+    player_2 = Player.new(params[:player_2_name])
+    @game = Game.create(player_1, player_2)
     redirect '/play'
   end
 
+  before do
+    @game = Game.instance
+  end
+
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.waiting_player)
     @game.turn
     redirect '/winner' if @game.winner
@@ -31,7 +33,6 @@ class Battle < Sinatra::Base
   end
 
   get '/winner' do
-    @game = $game
     erb :winner
   end
 
